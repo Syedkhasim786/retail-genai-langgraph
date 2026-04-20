1e01a16275d18959484eda045342ba8eb42bec3b
Syedkhasim786 <syedkhasim2001@gmail.com>
2026-04-20 12:46:08 +0530

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from graph.retail_graph import build_graph
+
+app = FastAPI()
+graph = build_graph()
+
+@app.get("/")
+def home():
+    return {"message": "Retail GenAI API running"}
+
+@app.get("/ask")
+def ask(query: str):
+    result = graph.invoke({"query": query})
+    return result
